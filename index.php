610650d7ebfae585645accfaa5ec876411a605ad
Judah <judah.ndivo@strathmore.edu>
2024-10-02 15:41:19 +0300

    Updated the index file

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  </head>
-  <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-  </body>
-</html>
+<?php
+require "load.php";
+$ObjLayouts->heading();
+$ObjMenus->main_menu();
+$ObjHeadings->main_banner();
+$ObjCont->main_content();
+$ObjCont->side_bar();
+$ObjLayouts->footer();
