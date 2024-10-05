f01bbeff83b56269f44ab53a782a07116ac77e82
Judah <judah.ndivo@strathmore.edu>
2024-10-05 15:03:12 +0300

    Updated the footer method

--- layouts/layout.php
+++ layouts/layout.php
@@ -17,3 +17,20 @@ class layouts {
 <body>
         <?php
     }
+    // Method to render the footer and closing tags
+    public function footer() {
+        ?>
+ 
+<footer class="pt-3 mt-4 text-body-secondary border-top">
+Copyright &copy; ICS <?php print date("Y"); ?>
+      </footer>
+    </div>
+  </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
+        <?php
+    }
+}
+?>
