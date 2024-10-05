38080c22e791906706d3fc338e833bd03363eaeb
Judah <judah.ndivo@strathmore.edu>
2024-10-05 14:51:25 +0300

    Sign Up Form HTML Structure

--- forms/user_forms.php
+++ forms/user_forms.php
@@ -2,5 +2,20 @@
 // Define the user_forms class
 class user_forms {
     // Sign up form method begins
-    public function sign_up_form($ObjGlob) {}
+    public function sign_up_form($ObjGlob) {
 ?>
+      <div class="row align-items-md-stretch">
+        <div class="col-md-9">
+          <div class="h-100 p-5 text-bg-dark rounded-3">
+            <h2>Sign Up</h2>
+            <?php
+            // Display messages and errors
+            print $ObjGlob->getMsg('msg');
+            $err = $ObjGlob->getMsg('errors');
+            ?>
+            <form action="<?php print basename($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data">
+                <div class="mb-3">
+                    <label for="fullname" class="form-label">Fullname:</label>
+                    <input type="text" name="fullname" class="form-control form-control-lg" maxlength="50" id="fullname" placeholder="Enter your name" <?php print (isset($_SESSION["fullname"])) ? 'value="'.$_SESSION["fullname"].'"'  : ''; unset($_SESSION["fullname"]); ?> >
+                    <?php print (isset($err['nameLetters_err'])) ? "<span class='invalid'>" . $err['nameLetters_err'] . "</span>" : '' ; ?>
+                </div>
