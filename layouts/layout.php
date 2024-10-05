2d7a78fdf313ff4f64539885253c1280083eec3d
Judah <judah.ndivo@strathmore.edu>
2024-10-05 15:02:31 +0300

    Class definition and heading method

---
+++ layouts/layout.php
@@ -0,0 +1,19 @@
+<?php
+
+class layouts {
+    // Method to render the HTML heading and opening tags
+    public function heading() {
+        ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="css/style.css">
+  
+</head>
+<body>
+        <?php
+    }
