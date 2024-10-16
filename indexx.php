ae5dd3e06c9394cb94801a3a04a4e4eb5965dc1e
Judah <judah.ndivo@strathmore.edu>
2024-10-16 06:21:03 +0300

    Included the includess folger in the indexx file

--- indexx.php
+++ indexx.php
@@ -1,1 +1,16 @@
+<?php include('includess/headerr.php'); ?>
 
+<div class="py-5"> 
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3>Login and Registration System in PHP</h3>
+                <h4>With Email Verification</h4>
+
+            </div>
+        </div>
+
+    </div>
+
+</div> 
+<?php include('includess/footerr.php'); ?>
